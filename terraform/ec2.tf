c3024971446ed2481fade476771da283d87280cd
root <root@DESKTOP-SH1QMTE>
2025-12-23 13:52:22 +0530

    update2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -29,8 +29,8 @@ resource "aws_security_group" "app_sg" {
 
 resource "aws_instance" "app" {
   count         = 2
-  ami           = "ami-0a91cd140a1fc148a"  # Amazon Linux 2
-  instance_type = "t3.micro"
+  ami           = "ami-02b8269d5e85954ef"  # Amazon Linux 2
+  instance_type = "m7i-flex.large"
   subnet_id     = element(aws_subnet.private.*.id, count.index)
   security_groups = [aws_security_group.app_sg.id]
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
